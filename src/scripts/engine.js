e8c6b668d5ceb421625e3b31e42b323761c49a9f
Gustavo A. Matos <gustavomatos2277@gmail.com>
2024-12-09 20:56:11 -0300

    Dinamização na criação dos cards

---
+++ src/scripts/engine.js
@@ -0,0 +1,29 @@
+const emojis = [
+    "💚",
+    "💚",
+    "🐷",
+    "🐷",
+    "⚽",
+    "⚽",
+    "🏆",
+    "🏆",
+    "🧠",
+    "🧠",
+    "🧊",
+    "🧊",
+    "🔥",
+    "🔥",
+    "🐽",
+    "🐽",
+];
+
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(()=>Math.random() > 0.5 ? 2 : -1)
+
+for(let i=0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box)
+}
